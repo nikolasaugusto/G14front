5a9c397d4f33851bd49271d7cb79a40de8d3cf7d
pedromurilo07 <pedro.almeida.1406157@sga.pucminas.br>
2024-11-02 16:54:29 -0300

    criado tela login e configurado rota

--- paginalogin/login.js
+++ paginalogin/login.js
@@ -8,20 +8,21 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     const errorMessage = document.getElementById("errorMessage");
   
     try {
-      const response = await fetch("https://api.seuservidor.com/login", {
+      const response = await fetch("http://localhost:5287/api/Usuarios/authenticate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email : email, senha : password})
       });
   
       const result = await response.json();
   
       if (response.ok) {
         // Sucesso no login
+        localStorage.setItem("jwtToken", result.jwtToken);
         alert("Login bem-sucedido!");
-        window.location.href = "/dashboard"; // Redireciona para a página inicial ou painel
+        window.location.href = "/G14front/paginausuario/index.html"; // Redireciona para a página inicial ou painel
       } else {
         // Exibe a mensagem de erro da API
         errorMessage.textContent = result.message || "Erro ao realizar o login.";
@@ -30,4 +31,11 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
       errorMessage.textContent = "Erro ao conectar com o servidor.";
     }
   });
+
+  document.getElementById("showPassword").addEventListener("change", function() {
+    const passwordField = document.getElementById("password");
+    passwordField.type = this.checked ? "text" : "password";
+});
+
+
   
